7996119649c95093fc84b36be7bba981090c763e
Sanemat <o.gata.ken@gmail.com>
2017-10-29 23:40:58 +0900

    feat(rename): dotfiles

--- update.js
+++ update.js
@@ -5,6 +5,7 @@ const copy = require('copy-concurrently');
 const uniqueString = require('unique-string');
 const tempDir = require('temp-dir');
 const move = require('move-concurrently');
+const globby = require('globby');
 
 const getPath = () => path.join(tempDir, uniqueString());
 
@@ -19,6 +20,13 @@ Promise.resolve().then(() => {
 }).then(() => {
   console.log(`removed ${copyDest}`);
   return move(path.join(tempDest, 'package.json'), path.join(tempDest, '_package.json'));
+}).then(() => {
+  return globby(['.*'], { cwd: tempDest });
+}).then((value) => {
+  console.log(value);
+  return Promise.all(value.map((dotFile) => {
+    return move(path.join(tempDest, dotFile), path.join(tempDest, dotFile.replace(/^./, '__')));
+  }));
 }).then(() => {
   return copy(tempDest, copyDest);
 }).then(() => {
