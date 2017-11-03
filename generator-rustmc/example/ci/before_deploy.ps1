b2cecfe2b7307739102a0c32d2aa646a57c272eb
Sanemat <o.gata.ken@gmail.com>
2017-11-03 16:53:01 +0900

    feat(crate): undescored crate name

--- generator-rustmc/example/ci/before_deploy.ps1
+++ generator-rustmc/example/ci/before_deploy.ps1
@@ -11,7 +11,7 @@ Set-Location $STAGE
 $ZIP = "$SRC_DIR\$($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME)-$($Env:TARGET).zip"
 
 # TODO Update this to package the right artifacts
-Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\example.exe" '.\'
+Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\example_foo.exe" '.\'
 Copy-Item "$SRC_DIR\changelog.md" '.\'
 Copy-Item "$SRC_DIR\readme.md" '.\'
 Copy-Item "$SRC_DIR\license-apache" '.\'
