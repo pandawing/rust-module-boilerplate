3cca66b1683aa323283c7c8b982f1bb3d2b94dd0
Sanemat <o.gata.ken@gmail.com>
2017-11-04 14:50:54 +0900

    chore(scripts): update copy logic

--- update.js
+++ update.js
@@ -11,7 +11,33 @@ const getPath = () => path.join(tempDir, uniqueString());
 
 let tempDest = getPath();
 console.log(tempDest);
-let copyDest = path.join('generator-rustmc', 'generators', 'app', 'templates');
+let copyDest = path.join('generator-rustm', 'generators', 'app', 'templates');
+Promise.resolve().then(() => {
+  return copy('boilerplate', tempDest);
+}).then(() => {
+  console.log(`copied to ${tempDest}`);
+  return pify(rimraf)(copyDest);
+}).then(() => {
+  console.log(`removed ${copyDest}`);
+  return move(path.join(tempDest, 'package.json'), path.join(tempDest, '_package.json'));
+}).then(() => {
+  return globby(['.*'], { cwd: tempDest });
+}).then((value) => {
+  console.log(value);
+  return Promise.all(value.map((dotFile) => {
+    return move(path.join(tempDest, dotFile), path.join(tempDest, dotFile.replace(/^./, '__')));
+  }));
+}).then(() => {
+  return copy(tempDest, copyDest);
+}).then(() => {
+  console.log('completed');
+}).catch(err => {
+  console.error(err);
+});
+
+tempDest = getPath();
+console.log(tempDest);
+copyDest = path.join('generator-rustmc', 'generators', 'app', 'templates');
 Promise.resolve().then(() => {
   return copy('cli-boilerplate', tempDest);
 }).then(() => {
